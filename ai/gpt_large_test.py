c7011c306222fd32794da6f62e8b71eb3d03711c
N01d3a <furdis11@gmail.com>
2025-02-28 20:44:14 +0100

    Fixed bugs and upgraded main

--- ai/gpt_large_test.py
+++ ai/gpt_large_test.py
@@ -1,7 +1,13 @@
-from transformers import GPT2Tokenizer, GPT2Model
+from transformers import GPT2Tokenizer, AutoModelForCausalLM, GPT2Model, AutoTokenizer
 
-tokenizer = GPT2Tokenizer.from_pretrained('gpt2-large')
-model = GPT2Model.from_pretrained('gpt2-large')
-text = "Replace me by any text you'd like."
-encoded_input = tokenizer(text, return_tensors='pt')
-output = model(**encoded_input)
+tokenizer = AutoTokenizer.from_pretrained('gpt2-large')
+model = AutoModelForCausalLM.from_pretrained('gpt2-large')
+prompt = "Replace me by any text you'd like."
+
+input_ids = tokenizer([prompt], return_tensors='pt')#.input_ids
+
+output = model.generate(**input_ids, max_new_tokens=100, do_sample=True)
+
+gen_text = tokenizer.batch_decode(output)[0]
+
+print(gen_text)
--- ai/test_gpt.py
+++ ai/test_gpt.py
@@ -1,16 +1,22 @@
+import warnings
+warnings.filterwarnings("ignore")
 from gpt4all import GPT4All
 
-def model():
-    model = GPT4All("Nous-Hermes-2-Mistral-7B-DPO.Q4_0.gguf")
-    return model
 
-def message(message_:str, title:str, tokens:int=2048):
-    model_ = model()
-    with model_.chat_session():
-        text = model_.generate(f"{message_}  Please rewrite this text, so that it is family friendly. Also rewrite any insults and make it readable for a text-to-speech ai, so convert any short forms like '18 Male' or AITA for 'Am I the Asshole' into a full text.", max_tokens=tokens)
-        title = model_.generate(f"{title}  Please rewrite this title, so that it is family friendly and attention grabbing as an Video title. Also rewrite any insults and make it readable for a text-to-speech ai, so convert any short forms like '18 Male' or AITA for 'Am I the Asshole' into a full word. Please only give me the title", max_tokens=tokens)
-    del model_
-    return text, title
+#.filterwarnings("ignore")
+
+def model():
+    with warnings.catch_warnings():
+        model = GPT4All("Nous-Hermes-2-Mistral-7B-DPO.Q4_0.gguf",  verbose=False)
+        return model
+
+def message(message_:str, title:str, model_:GPT4All, tokens:int=2048):
+    with warnings.catch_warnings():
+        with model_.chat_session():
+            text = model_.generate(f"{message_}  Please rewrite this text, so that it is family friendly. Also rewrite any insults and make it readable for a text-to-speech ai, so convert any short forms like '18 Male' or AITA for 'Am I the Asshole' into a full text.", max_tokens=tokens)
+            title = model_.generate(f"{title}  Please rewrite this title, so that it is family friendly and attention grabbing as an Video title. Also rewrite any insults and make it readable for a text-to-speech ai, so convert any short forms like '18 Male' or AITA for 'Am I the Asshole' into a full word. Please only give me the title", max_tokens=tokens)
+        #del model_
+        return text, title
 
 
 if __name__ == '__main__':
